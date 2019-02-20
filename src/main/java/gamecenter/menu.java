dd25f4a325933b49b96945e56599dd438773ee3e
Ceegan Hale <ceeganhale@gmail.com>
2019-02-19 19:36:22 -0700

    Creates the main menu

--- src/main/java/gamecenter/menu.java
+++
@@ -1,9 +0,0 @@
-package gamecenter;
-
-public class menu {
-
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-}
---
+++ src/main/java/menu.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class menu {
+
+    public static void main( String[] args )
+    {
+        int choice = 0;
+        while (choice != 4) {
+            choice = 0;
+            System.out.println(" ______     ______     __    __     ______        ______     ______     __   __     ______   ______     ______    \n" +
+                    "/\\  ___\\   /\\  __ \\   /\\ \"-./  \\   /\\  ___\\      /\\  ___\\   /\\  ___\\   /\\ \"-.\\ \\   /\\__  _\\ /\\  ___\\   /\\  == \\   \n" +
+                    "\\ \\ \\__ \\  \\ \\  __ \\  \\ \\ \\-./\\ \\  \\ \\  __\\      \\ \\ \\____  \\ \\  __\\   \\ \\ \\-.  \\  \\/_/\\ \\/ \\ \\  __\\   \\ \\  __<   \n" +
+                    " \\ \\_____\\  \\ \\_\\ \\_\\  \\ \\_\\ \\ \\_\\  \\ \\_____\\     \\ \\_____\\  \\ \\_____\\  \\ \\_\\\\\"\\_\\    \\ \\_\\  \\ \\_____\\  \\ \\_\\ \\_\\ \n" +
+                    "  \\/_____/   \\/_/\\/_/   \\/_/  \\/_/   \\/_____/      \\/_____/   \\/_____/   \\/_/ \\/_/     \\/_/   \\/_____/   \\/_/ /_/ \n"
+            );
+
+            System.out.println("Which game would you like to play?\n1. Gomuko\n2. Game 2\n3. Game 3\n4. Quit");
+
+            Scanner reader = new Scanner(System.in);
+            while (choice < 1 || choice > 4) {
+                System.out.print("> ");
+                if (reader.hasNextInt()){
+                    choice = reader.nextInt();
+                } else{
+                    reader.next();
+                }
+
+            }
+
+            reader.close();
+
+            if (choice == 1) {
+                ;
+            } else if (choice == 2) {
+                ;
+            } else if (choice == 3) {
+                ;
+            }
+        }
+    }
+}
