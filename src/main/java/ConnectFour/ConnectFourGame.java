bdaf8a33c86d2328a5ec0957eeeed8a121a4c8b3
Blaz Pocrnja <bpocrnja@gmail.com>
2019-02-25 11:47:12 -0700

    RED: Added Connect Four Game driver class tests.

---
+++ src/main/java/ConnectFour/ConnectFourGame.java
@@ -0,0 +1,9 @@
+package ConnectFour;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+public class ConnectFourGame {
+    public static void main(String[] args){
+        throw new NotImplementedException();
+    }
+}
--- src/main/java/ConnectFour/ConnectFourMain.java
+++
@@ -1,4 +0,0 @@
-package ConnectFour;
-
-public class ConnectFourMain {
-}
