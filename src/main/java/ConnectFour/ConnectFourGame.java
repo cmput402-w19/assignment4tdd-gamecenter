daab445a20c5ebed68ac1cdc4aa19af6b17dd80d
Blaz Pocrnja <bpocrnja@gmail.com>
2019-02-25 12:35:29 -0700

    GREEN: Implemented connect four game class.

--- src/main/java/ConnectFour/ConnectFourGame.java
+++ src/main/java/ConnectFour/ConnectFourGame.java
@@ -1,9 +1,142 @@
 package ConnectFour;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import java.util.Scanner;
 
+/**
+ * Contains connect four main method.
+ */
 public class ConnectFourGame {
-    public static void main(String[] args){
-        throw new NotImplementedException();
+    private ConnectFourBoard board;
+    private int ties;
+    private int numOfPlayerXWins;
+    private int numOfPlayerOWins;
+    private Scanner keyboardScanner;
+
+    /**
+     * Initializes a new instance of the ConnectFourGame class.
+     */
+    public ConnectFourGame() {
+        this.board = new ConnectFourBoard();
+        this.ties = 0;
+        this.numOfPlayerXWins = 0;
+        this.numOfPlayerOWins = 0;
+        this.keyboardScanner = new Scanner(System.in);
+    }
+
+    /**
+     * Loops player turns while reading user input until a winner is determined or the board is full.
+     *
+     * Based on TicTacToe "playGame" by Sarah Nadi:
+     * https://github.com/cmput402-w19/tictactoe
+     */
+    public void playGame() {
+        while (this.board.getWinner() == null) {
+            if (this.board.isFull()) {
+                break;
+            }
+
+            this.board.printBoard();
+            this.promptNextPlayer();
+
+            String line = this.keyboardScanner.nextLine();
+
+            try {
+                this.board.playMove(Integer.parseInt(line));
+            }
+            catch (InvalidMoveException e) {
+                System.out.println("Invalid column. Try again");
+            }
+            catch (NumberFormatException e) {
+                System.out.println("Invalid input. Try again");
+            }
+        }
+
+        resolveGame();
+    }
+
+    /**
+     * Prints the overall score.
+     */
+    public void printScoreBoard() {
+        System.out.println("\n-------------------------------------------");
+        System.out.println("S C O R E B O A R D");
+        System.out.println("-------------------------------------------");
+        System.out.println(String.format("X Wins: %d", this.numOfPlayerXWins));
+        System.out.println(String.format("O Wins: %d", this.numOfPlayerOWins));
+        System.out.println(String.format("Ties: %d", this.ties));
+        System.out.println("-------------------------------------------");
+    }
+
+    /**
+     * Prints a string to the command line prompting the current player for their column selection.
+     *
+     * Based on TicTacToe "promptNextPlayer" by Sarah Nadi:
+     * https://github.com/cmput402-w19/tictactoe
+     */
+    public void promptNextPlayer() {
+        System.out.println("It's player " + this.board.getSymbol(this.board.getCurrentPlayer()) + "'s turn. Please enter the column of your next move: ");
+    }
+
+    /**
+     * Creates a new instance of the game board.
+     */
+    public void resetGame() {
+        this.board = new ConnectFourBoard();
+    }
+
+    /**
+     * Prints the board and announces winner.
+     *
+     * Based on TicTacToe "resolveGame" by Sarah Nadi:
+     * https://github.com/cmput402-w19/tictactoe
+     */
+    private void resolveGame() {
+        this.board.printBoard();
+
+        ConnectFourBoard.Player winner = this.board.getWinner();
+
+        // Check who the winner is and print their corresponding winning message.
+        // If the winner is player X, increment X's score.
+        // If the winner is player O, increment O's score.
+        // Otherwise increment the number of ties.
+        if (winner == null) {
+            this.ties += 1;
+            System.out.println("Tie Game!");
+        } else {
+            if (winner == ConnectFourBoard.Player.X) this.numOfPlayerXWins += 1;
+            else if (winner == ConnectFourBoard.Player.O) this.numOfPlayerOWins += 1;
+            System.out.println("Player " + winner + " has won the game!");
+        }
+    }
+
+    /**
+     * Main static method for running connect four
+     * @param args command line arguments
+     *
+     * Based on TicTacToe "main" by Sarah Nadi:
+     * https://github.com/cmput402-w19/tictactoe
+     */
+    public static void main(String args[]) {
+        ConnectFourGame game = new ConnectFourGame();
+        boolean play = true;
+
+        while (play) {
+            game.playGame();
+
+            // Error check value till response is either y or n
+            String response = "";
+            while (!response.equalsIgnoreCase("y") && !response.equalsIgnoreCase("n")) {
+                System.out.print("Would you like to play again (y/n): ");
+                response = game.keyboardScanner.nextLine();
+            }
+
+            if (response.equalsIgnoreCase("n")) {
+                play = false;
+                game.printScoreBoard();
+            }
+            else {
+                game.resetGame();
+            }
+        }
     }
 }
